62fc0df4507ff8649a8ab9f5aa708b6f4dd00884
Rog <71998978+rogxo@users.noreply.github.com>
2023-01-19 19:00:54 +0800

    first upload
    for saving backup

---
+++ km/process.h
@@ -0,0 +1,129 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+//#include <asm/cpu.h>
+//#include <asm/io.h>
+
+#define ARC_PATH_MAX 256
+
+pid_t get_pid_by_name(char* name) {
+	
+    struct task_struct* task;
+    struct pid* pid_struct;
+    pid_t pid;
+    char comm[16];
+
+    for_each_process(task) {
+        get_task_comm(comm, task);
+        if (!strcmp(comm,name)) {
+            pid_struct = get_task_pid(task, PIDTYPE_PID);
+            pid = pid_nr(pid_struct);
+            //printk("%s[%d]\n", comm, pid);
+            return pid;
+        }
+    }
+    return 0;
+}
+
+void show_vma_attribute(struct vm_area_struct *vma) {
+    struct mm_struct *mm = vma->vm_mm;
+	struct file *file = vma->vm_file;
+	vm_flags_t flags = vma->vm_flags;
+	unsigned long ino = 0;
+	unsigned long long pgoff = 0;
+	unsigned long start, end;
+	dev_t dev = 0;
+	const char *name = NULL;
+    char buf[ARC_PATH_MAX];
+	char *path_nm = "";
+    char tmp_buffer[0xff];
+
+
+	if (file) {
+		struct inode *inode = file_inode(vma->vm_file);
+		dev = inode->i_sb->s_dev;
+		ino = inode->i_ino;
+		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+
+        path_nm = file_path(vma->vm_file, buf, ARC_PATH_MAX-1);
+	}
+
+	start = vma->vm_start;
+	end = vma->vm_end;
+
+	if (vma->vm_ops && vma->vm_ops->name) {
+		name = vma->vm_ops->name(vma);
+		if (name)
+			goto done;
+	}
+
+	//name = arch_vma_name(vma);
+	if (!name) {
+        
+		if (!mm) {
+			name = "[vdso]";
+			goto done;
+		}
+
+		if (vma->vm_start <= mm->brk &&
+		    vma->vm_end >= mm->start_brk) {
+			name = "[heap]";
+			goto done;
+		}
+
+		if (vma->vm_start <= vma->vm_mm->start_stack &&
+		    vma->vm_end >= vma->vm_mm->start_stack) {
+			name = "[stack]";
+			goto done;
+		}
+
+		if (!vma->vm_file && vma->anon_name) {
+			sprintf(tmp_buffer, "[anon:%s]", vma->anon_name->name);
+            name = tmp_buffer;
+            goto done;
+		}
+
+        name = "";
+	}
+
+done:
+    printk("%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu  \t\t  %s %s",
+            start,
+            end,
+            flags & VM_READ ? 'r' : '-',
+            flags & VM_WRITE ? 'w' : '-',
+            flags & VM_EXEC ? 'x' : '-',
+            flags & VM_MAYSHARE ? 's' : 'p',
+            pgoff,
+            MAJOR(dev), MINOR(dev), ino,
+            path_nm,
+            name);
+}
+
+
+uintptr_t get_module_base(pid_t pid, char* name) {
+
+	struct pid* pid_struct;
+    struct task_struct* task;
+	struct mm_struct* mm;
+    struct vm_area_struct *vma;
+
+    pid_struct = find_get_pid(pid);
+    if (!pid_struct) {
+        return false;
+    }
+	task = get_pid_task(pid_struct, PIDTYPE_PID);
+	if (!task)
+        return false;
+
+	mm = get_task_mm(task);
+    if (!mm)
+        return false;
+
+    for (vma = mm->mmap; vma; vma = vma->vm_next) {
+        show_vma_attribute(vma);
+        //To be continue......
+    }
+
+    return 0;
+}
+
---
+++ um/driver.hpp
@@ -0,0 +1,105 @@
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+
+#define DEVICE_NAME "/dev/my_misc_device"
+
+class c_driver {
+private:
+    int fd;
+    pid_t pid;
+
+    typedef struct _COPY_MEMORY {
+        pid_t pid;
+        uintptr_t addr;
+        void* buffer;
+        size_t size;
+    } COPY_MEMORY, *PCOPY_MEMORY;
+
+    typedef struct _MODULE_BASE {
+        pid_t pid;
+        char* name;
+        uintptr_t base;
+    } MODULE_BASE, *PMODULE_BASE;
+
+    enum OPERATIONS {
+        OP_READ_MEM = 0x801,
+        OP_WRITE_MEM = 0x802,
+        OP_MODULE_BASE = 0x803,
+    };
+
+public:
+    c_driver() {
+        fd = open(DEVICE_NAME, O_RDWR);
+        if (fd == -1) {
+            printf("[-] open driver failed\n");
+        }
+    }
+
+    ~c_driver() {
+        //wont be called
+        if (fd > 0)
+            close(fd);
+    }
+
+    void initialize(pid_t pid) {
+        this->pid = pid;
+    }
+
+    uintptr_t get_module_base(char* name) {
+        MODULE_BASE mb;
+
+        mb.pid = this->pid;
+        mb.name = name;
+
+        if (ioctl(fd, OP_MODULE_BASE, &mb) != 0) {
+            printf("[-] ioctl failed\n");
+            return 0;
+        }
+        return mb.base;
+    }
+
+    bool read(uintptr_t addr, void *buffer, size_t size) {
+        COPY_MEMORY cm;
+
+        cm.pid = this->pid;
+        cm.addr = addr;
+        cm.buffer = buffer;
+        cm.size = size;
+
+        if (ioctl(fd, OP_READ_MEM, &cm) != 0) {
+            printf("[-] ioctl failed\n");
+            return false;
+        }
+        return true;
+    }
+
+    bool write(uintptr_t addr, void *buffer, size_t size) {
+        COPY_MEMORY cm;
+
+        cm.pid = this->pid;
+        cm.addr = addr;
+        cm.buffer = buffer;
+        cm.size = size;
+
+        if (ioctl(fd, OP_WRITE_MEM, &cm) != 0) {
+            printf("[-] ioctl failed\n");
+            return false;
+        }
+        return true;
+    }
+
+    template <typename T>
+    T read(uintptr_t addr) {
+        T res;
+        if (this->read(addr, &res, sizeof(T)))
+            return res;
+        return {};
+    }
+
+    template <typename T>
+    bool write(uintptr_t addr,T value) {
+        return this->write(addr, &value, sizeof(T));
+    }
+};
+
+static c_driver *driver = new c_driver();
